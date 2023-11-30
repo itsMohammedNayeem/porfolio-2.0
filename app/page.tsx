8e7f790703e4b0931b9a213ca46225d8a137473f
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-11-30 07:06:08 +0530

    Get in touch fixed and scroll to top added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
+import Link from "next/link";
+import { ChevronDoubleUpIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
@@ -34,6 +36,20 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            {/* <img
+              src="http://www.w3.org/2000/svg"
+              alt="arrow-up"
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+            /> */}
+
+            <ChevronDoubleUpIcon className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" />
+          </div>
+        </footer>
+      </Link>
     </main>
   );
 }
