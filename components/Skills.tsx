028fbded85c36c44da42e98773a7458d34f35f39
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-12-04 22:56:42 +0530

    Update skill component styles

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -22,7 +22,7 @@ const Skills = ({ skills }: Props) => {
 
       <h3 className='absolute top-36 uppercase tracking-[3px]'>Hover over a skill for current proficiency</h3>
 
-      <div className='grid grid-cols-4 gap-5'>
+      <div className='absolute top-[205px] grid grid-cols-4 lg:grid-cols-8 gap-1 md:gap-5'>
         {skills?.slice(0, skills.length / 2).map(skill => <Skill key={skill._id} skill={skill} />)}
 
         {/* Get second half of skills and map with direction left */}
