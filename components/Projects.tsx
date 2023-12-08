f8b2a110a2ee6c49d341768908aab906526cb605
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-12-08 15:41:18 +0530

    Refactor Projects component styles

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,22 +18,22 @@ const Projects = ({ projects }: Props) => {
       <h3 className='absolute top-20 text-2xl uppercase tracking-[20px] text-gray-500 md:top-24'>Projects</h3>
 
       <div className='relative z-20 flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-sun'>
-        {projects?.map((project) => (
+        {projects?.map(project => (
           <div
             key={project.projectId}
-            className='flex w-screen h-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44'>
+            className='flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44'>
             <motion.img
               initial={{ opacity: 0, y: -300 }}
               transition={{ duration: 1.2 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
               src={urlFor(project?.image).url()}
-              className='h-[250px] w-[350px] md:h-[350px] md:w-[500px] object-cover rounded-xl shadow-xl'
+              className='h-[250px] w-[350px] rounded-xl object-cover shadow-xl md:h-[350px] md:w-[500px]'
               alt=''
             />
 
-            <div className='max-w-6xl space-y-3 md:space-y-5 px-0 md:px-10'>
-              <h4 className='text-center text-1xl font-semibold'>
+            <div className='max-w-6xl space-y-3 px-0 md:space-y-5 md:px-10'>
+              <h4 className='text-1xl text-center font-semibold'>
                 <span className='underline decoration-sun underline-offset-4'>
                   Project {project.projectId} of {projects.length}:
                 </span>{' '}
@@ -45,7 +45,7 @@ const Projects = ({ projects }: Props) => {
                   {project.technologies.map(technology => (
                     <img
                       key={technology._id}
-                      className='h-6 w-6 md:h-10 md:w-10 rounded-full'
+                      className='h-6 w-6 rounded-full md:h-10 md:w-10'
                       src={urlFor(technology.image).url()}
                       alt={technology.title}
                     />
